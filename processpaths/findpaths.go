b7648ef45a834d503b61257ac2801bf47e33031d
David Ochiel <nguonodave@gmail.com>
2024-12-17 06:29:36 +0300

    chore(comments): add comments to the FindPaths (DFS) function for clarity

--- processpaths/findpaths.go
+++ processpaths/findpaths.go
@@ -11,6 +11,11 @@ var (
 	current string
 )
 
+// FindPaths recursively searches for all possible paths from the 'start' room to the 'end' room.
+// The function tracks the current path and visited rooms to avoid revisiting them.
+// If the 'current' room is the same as the 'end' room, the current path is saved to vars.AllPaths.
+// The function explores each neighboring room, and for unvisited rooms, it recursively calls itself to continue the search.
+// After exploring all neighbors, it backtracks by removing the last room from the path and visited lists.
 func FindPaths(start, end string) {
 	visited = append(visited, start)
 	path = append(path, start)
